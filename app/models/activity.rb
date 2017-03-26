c9f782fba130bf78b7210943288244801243e10a
Musa Abdillah <musaabdillahm9@gmail.com>
2017-03-26 17:05:23 +0700

    deploy time

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ActiveRecord::Base
   belongs_to :company
-  has_attached_file :image, styles: { large: "500x500", medium: "300x300", thumb: "100x100" }
+  has_attached_file :image, styles: { large: "500x250", medium: "300x300", thumb: "100x100" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
